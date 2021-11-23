845a2e8a34ebe3ad605b801d457f6a0cb487f1fc
rami kadaweh <ramikadaweh31@gmail.com>
2021-11-23 22:13:48 +0200

    step7 complited

---
+++ javascript-basics-01/Step_7.html + Step_7.js
@@ -0,0 +1,6 @@
+var shoe=prompt("enter your shoe number");
+var birth=prompt("enter your birth year");
+function res(){
+result=(((((shoe*2)+5)*50)-birth)+1766);
+alert("result is "+result);}
+res();
