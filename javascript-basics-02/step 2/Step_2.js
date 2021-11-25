25579b1f637eabb21fc4febe55b27143cb560ed3
rami kadaweh <ramikadaweh31@gmail.com>
2021-11-25 20:43:54 +0200

    step2 02 completed

---
+++ javascript-basics-02/step 2/Step_2.js
@@ -0,0 +1,6 @@
+var inp=document.querySelector("#name");
+inp.addEventListener("blur",unfocuse);
+function unfocuse(){
+    document.getElementById('name');
+    alert("thank you for participating!");
+}
