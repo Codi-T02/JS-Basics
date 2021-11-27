ee81c284f514e6238f68f3495da34dea05df0be9
rami kadaweh <ramikadaweh31@gmail.com>
2021-11-27 15:22:48 +0200

    step3 03 completed

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,11 @@
+function fun(nbr){
+    if (nbr===1){
+        document.getElementById('text').style.color="green";
+    }
+    if (nbr===2){
+        document.getElementById('text').style.color="red";
+    }
+    if (nbr===3){
+        document.getElementById('text').style.color="blue";
+    }
+}
