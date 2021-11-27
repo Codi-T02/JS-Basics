96df824425de924ca4a28b29fbf6dd3b95e9dbeb
rami kadaweh <ramikadaweh31@gmail.com>
2021-11-27 15:07:18 +0200

    step2 03 completed

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,7 @@
+function fun(nbr) {
+    if (nbr === 2) {
+        document.getElementById('texte').style.display = "none";
+    } else {
+        document.getElementById('texte').style.display = "block";
+    }
+}
